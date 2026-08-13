42838dd70281932f4a01355953c5cbbeecec1d30
Arthur <arthur.9996@gmail.com>
2026-08-13 19:24:56 +0200

    ➕ dep : nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxt-auth-utils']
+  modules: ['@nuxt/eslint', 'nuxt-auth-utils', '@nuxtjs/seo']
 })
