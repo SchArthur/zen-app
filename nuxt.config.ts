04dee7c87392e10b1b69bbee721f2281a32f0146
Arthur <arthur.9996@gmail.com>
2026-08-16 15:33:28 +0200

    ➕ dep : tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
   modules: ['@nuxt/eslint', 'nuxt-auth-utils', '@nuxtjs/seo'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   // @nuxtjs/seo compose les titres avec « %s | %siteName » : sans nom de site,
   // le jeton reste affiché tel quel dans l'onglet du navigateur.
